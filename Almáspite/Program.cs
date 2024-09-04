83f48c466921a16115bc416290bb301268347bd2
labadi.balint.jozsef <labadi.balint.jozsef@DESKTOP-OQP7BM8>
2024-09-04 13:44:11 +0200

    Add project files.

---
+++ Almáspite/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almáspite
+{
+    internal class Program
+    {
+        Program()
+            {
+             Console.WriteLine("Hello world")
+            }
+        static void Main(string[] args)
+        {
+        new Program();
+        }
+    }
+}
